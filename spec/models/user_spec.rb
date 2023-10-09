6e9e2e3da54fd6aa3911e7d44ecbe3fdd781d69b
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-09 19:35:52 +0200

    Add user name must be pleasent spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :Model do
+  let(:user) {User.new(name: 'Manzi')}
+  before {user.save}
+
+  it "name should be present" do 
+    user.name = nil 
+    expect(user).to_not be_valid
+  end
 end
