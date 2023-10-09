7359d7b2efd9cbb63dbfe5376ea56767a2d668b7
kessie <kayp3477@gmail.com>
2023-10-09 18:07:57 +0000

    Add RSpec tests for InventoryFood model validation.

--- spec/models/inventory_food_spec.rb
+++ spec/models/inventory_food_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe InventoryFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:inventory) { Inventory.create(name: 'Sample Inventory') }
+  let(:food) { Food.create(name: 'Sample Food') }
+
+  it 'is valid with valid attributes' do
+    inventory_food = InventoryFood.new(
+      inventory: inventory,
+      food: food,
+      quantity: 10
+    )
+    expect(inventory_food).to be_valid
+  end
+
+  it 'is not valid without a quantity' do
+    inventory_food = InventoryFood.new(
+      inventory: inventory,
+      food: food
+    )
+    expect(inventory_food).not_to be_valid
+  end
+
+  it 'is not valid if quantity is not a positive integer' do
+    inventory_food = InventoryFood.new(
+      inventory: inventory,
+      food: food,
+      quantity: -5
+    )
+    expect(inventory_food).not_to be_valid
+
+    inventory_food.quantity = 5.5
+    expect(inventory_food).not_to be_valid
+  end
 end
