bed2abdcf083640e3d3638d566000abd04cd2c83
kessie <kayp3477@gmail.com>
2023-10-09 18:09:27 +0000

    Add RSpec tests for InventoryFood model validation.

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John') }
+
+  it 'is valid with valid attributes' do
+    inventory = Inventory.new(
+      name: 'Sample Inventory',
+      user: user
+    )
+    expect(inventory).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    inventory = Inventory.new(
+      user: user
+    )
+    expect(inventory).not_to be_valid
+  end
+
+  it 'is not valid without a user_id' do
+    inventory = Inventory.new(
+      name: 'Sample Inventory'
+    )
+    expect(inventory).not_to be_valid
+  end
+
+  it 'is not valid if name is too long' do
+    inventory = Inventory.new(
+      name: 'A' * 256,
+      user: user
+    )
+    expect(inventory).not_to be_valid
+  end
 end
