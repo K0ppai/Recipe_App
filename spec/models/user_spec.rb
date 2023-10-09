bf91fbc33716aa3327701c516a36fa1d286c1123
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-09 19:40:14 +0200

    Add relationship specs in User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe User, type: :Model do
     user.name = nil 
     expect(user).to_not be_valid
   end
+  it "it has many recipes" do 
+    relationship = described_class.reflect_on_association(:recipes)
+      expect(relationship.macro).to eq :has_many 
+  end
+  it "it has many inventories" do 
+    relationship = described_class.reflect_on_association(:inventories)
+      expect(relationship.macro).to eq :has_many 
+  end
 end
