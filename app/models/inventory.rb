122a75d3e12d3c8536ec89890a86c03c23d6a0ee
K0ppai <psthu.koppai@gmail.com>
2023-10-09 22:26:51 +0630

    Fix rubocop errors

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,4 @@
 class Inventory < ApplicationRecord
-  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :inventory_foods
 end
