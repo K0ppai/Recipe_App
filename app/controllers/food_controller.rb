fab7cf437ced77337fc97338ab3446d0e4da883e
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-09 20:54:51 +0200

    Add all actions in controller

---
+++ app/controllers/food_controller.rb
@@ -0,0 +1,15 @@
+class FoodController < ApplicationController
+    def index
+        @foods = Food.all
+    end 
+    def show
+        @food = Food.find(params[:id])
+    end
+    def new
+        @food =  Food.new
+    end
+    def create     
+    end
+    def destroy
+    end
+end
