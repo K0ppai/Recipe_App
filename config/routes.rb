12788ada1b8f9ddc6feaa41e02d38684626d4085
kessie <kayp3477@gmail.com>
2023-10-10 23:11:00 +0000

    Add inventory routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   resources :foods
+
+  resources :inventories do
+    resources :inventory_foods, only: %i[new create destroy]
+  end
+
+  get 'inventories/new', to: 'inventories#new'
+  post 'inventories/create', to: 'inventories#create'
 end
