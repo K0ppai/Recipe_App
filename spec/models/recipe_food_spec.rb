874977f3b08da9bf6a2a6c7faa02d2e3afa703a8
K0ppai <psthu.koppai@gmail.com>
2023-10-10 11:37:49 +0630

    Fix rubocop errors

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,25 +1,25 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
   before :each do
-    user = User.new(name: "koppai")
-    recipe = Recipe.new(name: "food", preparation_time: 20, cooking_time: 10, description: "this is how", public: false, user:)
-    food = Food.new(name: "rice", measurement_unit: "g", price: 20)
+    user = User.new(name: 'koppai')
+    recipe = Recipe.new(name: 'food', preparation_time: 20, cooking_time: 10, description: 'this is how', public: false, user:)
+    food = Food.new(name: 'rice', measurement_unit: 'g', price: 20)
     @recipe_food = RecipeFood.new(recipe:, food:, quantity: 12)
   end
 
-  context "When creating a new recipe_food" do
-    it "quantity should not be blank" do
+  context 'When creating a new recipe_food' do
+    it 'quantity should not be blank' do
       @recipe_food.quantity = nil
       expect(@recipe_food).not_to be_valid
     end
 
-    it "quantity should not be negative" do
+    it 'quantity should not be negative' do
       @recipe_food.quantity = -12
       expect(@recipe_food).not_to be_valid
     end
 
-    it "without belongs_to associations, it should be invalid" do
+    it 'without belongs_to associations, it should be invalid' do
       @recipe_food.food = nil
       expect(@recipe_food).not_to be_valid
     end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -6,33 +6,33 @@ RSpec.describe Recipe, type: :model do
     @recipe = Recipe.new(name: 'food', preparation_time: 20, cooking_time: 10, description: 'this is how', public: false, user: @user)
   end
 
-  context "When creating a new recipe" do
-    it "name should not be blank" do
+  context 'When creating a new recipe' do
+    it 'name should not be blank' do
       @recipe.name = nil
       expect(@recipe).not_to be_valid
     end
-    
-    it "name length should be between 1 and 100" do
+
+    it 'name length should be between 1 and 100' do
       @recipe.name = 'a' * 120
       expect(@recipe).not_to be_valid
     end
 
-    it "preparation_time should not be blank" do
+    it 'preparation_time should not be blank' do
       @recipe.preparation_time = nil
       expect(@recipe).not_to be_valid
     end
 
-    it "cooking_time should not be blank" do
+    it 'cooking_time should not be blank' do
       @recipe.cooking_time = nil
       expect(@recipe).not_to be_valid
     end
 
-    it "description should not be blank" do
+    it 'description should not be blank' do
       @recipe.description = nil
       expect(@recipe).not_to be_valid
     end
 
-    it "public should not be blank" do
+    it 'public should not be blank' do
       @recipe.public = nil
       expect(@recipe).not_to be_valid
     end
