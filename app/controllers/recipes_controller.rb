c57608fec1491dda2a5e19c816f2ab257fb5599e
K0ppai <psthu.koppai@gmail.com>
2023-10-10 20:24:41 +0630

    Add new and create actions in RecipeController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,10 +9,28 @@ class RecipesController < ApplicationController
   end
 
   def shopping_list
-    
   end
 
   def modal
-    
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
