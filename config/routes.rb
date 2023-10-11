8a0a11f08b54cae558dfb3466277f734f0f68b05
K0ppai <psthu.koppai@gmail.com>
2023-10-11 19:40:49 +0630

    Fix rubocop error

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipes_foods, only: %i[new create destroy]
+  end
 
   resources :inventories do
     resources :inventory_foods, only: %i[new create destroy]
