ffb3ba99055fd8c33ee709bbba090c129488de27
K0ppai <psthu.koppai@gmail.com>
2023-10-11 18:46:26 +0630

    Add foodcounter column in recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_105951) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_115834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_105951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "totalvaluecounter", default: 0
+    t.integer "foodcounter", default: 0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
