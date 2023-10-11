2e859842f1b5999a2bea9a055eca70ef188ff948
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-12 00:18:32 +0200

    Add functionality in Create action food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,7 +11,20 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
-  def create; end
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
 
   def destroy; end
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+
 end
