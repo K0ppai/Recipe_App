2344320964c78f7b7748c51aa0373f9e5d71934c
K0ppai <psthu.koppai@gmail.com>
2023-10-11 16:28:38 +0630

    Add destroy action in RecipeFoodsController

--- app/controllers/recipes_foods_controller.rb
+++ app/controllers/recipes_foods_controller.rb
@@ -10,15 +10,22 @@ class RecipesFoodsController < ApplicationController
     @recipe_food.recipe = @recipe
 
     if @recipe_food.save
-      redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe), notice: 'RecipeFood was successfully created.'
     else
       render :new, alert: "Something went wrong"
     end
   end
 
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food.destroy
+    redirect_to recipe_path(id: @recipe), notice: 'RecipeFood deleted successfully!'
+  end
+  
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity, food_attributes: [:name, :measurement_unit, :price])
+    params.require(:recipe_food).permit(:quantity, :food_id, food_attributes: [:name, :measurement_unit, :price])
   end
 end
