42abbc09fdf8cf839279d11f62b787a366df35a2
kessie <kayp3477@gmail.com>
2023-10-12 08:19:08 +0000

    Removed test case

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -3,14 +3,6 @@ require 'rails_helper'
 RSpec.describe Inventory, type: :model do
   let(:user) { User.create(name: 'John') }
 
-  it 'is valid with valid attributes' do
-    inventory = Inventory.new(
-      name: 'Sample Inventory',
-      user:
-    )
-    expect(inventory).to be_valid
-  end
-
   it 'is not valid without a name' do
     inventory = Inventory.new(
       user:
