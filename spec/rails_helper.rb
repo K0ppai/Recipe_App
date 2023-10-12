a035fbb985d2ce3ac0c70d1cc7244e3e8a25b259
kessie <kayp3477@gmail.com>
2023-10-12 22:49:20 +0000

    Add specs configurtions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Capybara::DSL, type: :feature
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
