a4ce8f1fe92a650949f6e933f18384c9444e58f0
K0ppai <psthu.koppai@gmail.com>
2023-10-13 12:05:29 +0630

    Add tests for recipes and recipes_food routes

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -7,14 +7,13 @@ RSpec.describe 'Recipes', type: :request do
   describe 'GET /recipes' do
     before :each do
       sign_in user
+      get recipes_path
     end
     it 'returns http success' do
-      get recipes_path
       expect(response).to have_http_status(:success)
     end
 
     it 'should render the correct template' do
-      get recipes_path
       expect(response).to render_template(:index)
     end
   end
@@ -22,15 +21,29 @@ RSpec.describe 'Recipes', type: :request do
   describe 'GET /recipes/:id' do
     before :each do
       sign_in user
+      get recipe_path(recipe)
     end
     it 'returns http success' do
-      get recipe_path(recipe)
       expect(response.status).to be 200
     end
 
     it 'should render the correct template' do
-      get recipe_path(recipe)
       expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET /recipes/new' do
+    before :each do
+      sign_in user
+      get new_recipe_path
+    end
+    
+    it 'returns http success' do
+      expect(response.status).to be 200
+    end
+    
+    it 'should render the correct template' do
+      expect(response).to render_template(:new)
     end
   end
 end
